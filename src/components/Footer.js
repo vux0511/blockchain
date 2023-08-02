e438318af83fb444e870e1413ccbc784d9ffa4ac
vux <vux.0511@gmail.com>
2023-08-02 11:27:14 +0700

    rename file

---
+++ src/components/Footer.js
@@ -0,0 +1,79 @@
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="container footer__container">
+                <ul className="footer__list">
+                    <li className="footer__item">
+                        <img
+                            src="images/logo.png"
+                            alt="Logo"
+                            className="footer__logo"
+                        />
+                        <div className="footer__desc">
+                            The best NFT marketplace website in the world and
+                            feel your experience in selling or buy our work
+                        </div>
+                        <div className="footer__social">
+                            <img
+                                src="images/youtube.svg"
+                                alt=""
+                                className="footer__social-youtube"
+                            />
+                            <img
+                                src="images/discord.svg"
+                                alt=""
+                                className="footer__social-discord"
+                            />
+                            <img
+                                src="images/instagram.svg"
+                                alt=""
+                                className="footer__social-instagram"
+                            />
+                        </div>
+                    </li>
+                    <li className="footer__item">
+                        <div className="footer__title">About</div>
+                        <a href="#" className="footer__link">
+                            Product
+                        </a>
+                        <a href="#" className="footer__link">
+                            Resource
+                        </a>
+                        <a href="#" className="footer__link">
+                            Term &amp; Condition
+                        </a>
+                        <a href="#" className="footer__link">
+                            FAQ
+                        </a>
+                    </li>
+                    <li className="footer__item">
+                        <div className="footer__title">Company</div>
+                        <a href="#" className="footer__link">
+                            Our Tea
+                        </a>
+                        <a href="#" className="footer__link">
+                            Partner With Us
+                        </a>
+                        <a href="#" className="footer__link">
+                            Term &amp; Condition
+                        </a>
+                        <a href="#" className="footer__link">
+                            Features
+                        </a>
+                    </li>
+                    <li className="footer__item">
+                        <div className="footer__title">Contact</div>
+                        <a href="#" className="footer__link">
+                            +012 3456789
+                        </a>
+                        <a href="#" className="footer__link">
+                            vux.0511@gmail.com
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.png";
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="header__list">
+                <img src={Logo} alt="Logo" className="header__logo" />
+                <ul className="header__menu">
+                    <li className="header__item">
+                        <Link to="marketplace" className="header__link">
+                            {" "}
+                            Marketplace{" "}
+                        </Link>
+                    </li>
+                    <li className="header__item">
+                        <a href="#" className="header__link">
+                            {" "}
+                            Collections{" "}
+                        </a>
+                    </li>
+                    <li className="header__item">
+                        <a href="#" className="header__link">
+                            {" "}
+                            Creators{" "}
+                        </a>
+                    </li>
+                    <li className="header__item">
+                        <a href="#" className="header__link">
+                            {" "}
+                            Stats{" "}
+                        </a>
+                    </li>
+                    <div className="header__icon">
+                        <ion-icon
+                            name="search-outline"
+                            className="header__icon-search"
+                        />
+                        <ion-icon
+                            name="cart-outline"
+                            className="header__icon-cart"
+                        />
+                    </div>
+                    <li className="header__item">
+                        <div className="header__button">
+                            <a href="#" className="button button-primary">
+                                Create
+                            </a>
+                            <a href="#" className="button button-primary">
+                                Connect Wallet
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+                <i className="fa-solid fa-bars-staggered menu-toggle" />
+            </div>
+        </header>
+    );
+}
+
+export default Header;
